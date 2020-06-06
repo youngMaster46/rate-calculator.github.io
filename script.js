0222dc24bbd31b94977dc1d1c3623430e65744b3
Maxim Kaplin <kaplin95@inbox.ru>
2020-06-06 11:45:52 +0300

    prepare scripts

---
+++ script.js
@@ -0,0 +1,19 @@
+const currencyEl_one = document.getElementById('currency-one')
+const amountEl_one = document.getElementById('amount-one')
+const currencyEl_two = document.getElementById('currency-two')
+const amountEl_two = document.getElementById('amount-two')
+
+const rateEl = document.getElementById('rate')
+const swap = document.getElementById('swap')
+
+// Fetch exchange rates and update the DOM
+function calculate() {
+    console.log("work");
+}
+
+
+// Event listeners
+currencyEl_one.addEventListener('change', calculate)
+amountEl_one.addEventListener('input', calculate)
+currencyEl_two.addEventListener('change', calculate)
+amountEl_two.addEventListener('input', calculate)
